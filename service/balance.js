6748545090626ab0f507ae5a1a0e77c8e2e4e1f0
Gabriel Lima <gabrielhung8@gmail.com>
2022-06-04 12:12:56 -0300

    Refatorando

--- service/balance.js
+++ service/balance.js
@@ -1,136 +1,42 @@
 "use strict"
 export const balance = () => {
-    const store1 = document.querySelector(".tableReceiptStore1");
-    const store2 = document.querySelector(".tableReceiptStore2");
-    const expensesTable = document.querySelector(".expensesTable");
-    const totalStore1 = store1.querySelectorAll(".total");
-    const totalStore2 = store2.querySelectorAll(".total");
-    const qtClient1 = store1.querySelectorAll(".qtClient");
-    const qtClient2 = store2.querySelectorAll(".qtClient");
-    const totalExpenses = expensesTable.querySelectorAll(".totalExpense");
-    
-    
-    let totalMonthStore1 = 0;
-    let totalMonthStore2 = 0;
-    let totalClientStore1 = 0;
-    let totalClientStore2 = 0;
-    let totalExpenseMonth = 0;
-
-    
-    //
-    for (let i = 0; i < totalStore1.length; i++) {
-
-        
-        const total = totalStore1[i];
-        const totalValue = total.textContent
-        const valor = parseFloat(totalValue);
-    
-        
-        totalMonthStore1 += valor;   
-
-    }
-
-    let totalMonth1 = parseFloat(totalMonthStore1).toFixed(2);
-        
+    // estruturas
+    const totalStore1 = document.querySelector(".tableReceiptStore1").querySelectorAll(".total");
+    const totalStore2 = document.querySelector(".tableReceiptStore2").querySelectorAll(".total");
+    const qtClient1 = document.querySelector(".tableReceiptStore1").querySelectorAll(".qtClient");
+    const qtClient2 = document.querySelector(".tableReceiptStore2").querySelectorAll(".qtClient");
+    const totalExpenses = document.querySelector(".expensesTable").querySelectorAll(".totalExpense");
+    // estruturas no balanço
     const tableTotalStore1 = document.querySelector(".totalStore1");
-    if(totalStore1.length == 0){
-        tableTotalStore1.innerHTML = "0";
-    }
-    else{
-        tableTotalStore1.innerHTML = totalMonth1;
-    }
-    //
-    //
-    for (let i = 0; i < totalStore2.length; i++) {
-
-        
-        const total = totalStore2[i];
-        const totalValue = total.textContent
-        const valor = parseFloat(totalValue);
-    
-        
-        totalMonthStore2 += valor;   
-        
-    }
-
-    let totalMonth2 = parseFloat(totalMonthStore2).toFixed(2);
-        
     const tableTotalStore2 = document.querySelector(".totalStore2");
-    if(totalStore2.length == 0){
-        tableTotalStore2.innerHTML = "0";
-    }
-    else{
-        tableTotalStore2.innerHTML = totalMonth2;
-    }
-    //
-    //
-    for (let i = 0; i < totalExpenses.length; i++) {
-
-        
-        const total = totalExpenses[i];
-        const totalValue = total.textContent;
-        const valor = parseFloat(totalValue);
-    
-        
-        totalExpenseMonth += valor;   
-
-        
-        
-    }
-    let expenseTotal = parseFloat(totalExpenseMonth).toFixed(2);
     const tableTotalExpense = document.querySelector(".total-expense");
-    if(totalExpenses.length == 0){
-        tableTotalExpense.innerHTML = "0";
-    }
-    else{
-        tableTotalExpense.innerHTML = expenseTotal;
-    }
-    //
-    for (let i = 0; i < qtClient1.length; i++) {
-
-        
-        const total = qtClient1[i];
-        const totalValue = total.textContent;
-        const valor = parseInt(totalValue);
-    
-        
-        totalClientStore1 += valor;   
-
-    }
-    let clientTotal = parseInt(totalClientStore1);
-        
     const tableTotalClient1 = document.querySelector(".qt1");
-    if(qtClient1.length == 0){
-        tableTotalClient1.innerHTML = "0";
-    }
-    else{
-        tableTotalClient1.innerHTML = clientTotal;
-    }
-    //
-    for (let i = 0; i < qtClient2.length; i++) {
-
-        
-        const total = qtClient2[i];
-        const totalValue = total.textContent;
-        const valor = parseInt(totalValue);
-    
-        
-        totalClientStore2 += valor;   
-        
-    }
-    let clientTotal2 = parseInt(totalClientStore2);
-        
     const tableTotalClient2 = document.querySelector(".qt2");
-    if(qtClient2.length == 0){
-        tableTotalClient2.innerHTML = "0";
-    }
-    else{
-        tableTotalClient2.innerHTML = clientTotal2;
-    }
+    let tableBalance = document.querySelector(".balance");
+    // calcula totais
+    let totalMonthStore1 = calcTotalMoney(totalStore1);
+    let totalMonthStore2 = calcTotalMoney(totalStore2);
+    let totalClientStore1 = calcTotalQt(qtClient1);
+    let totalClientStore2 = calcTotalQt(qtClient2);
+    let totalExpenseMonth = calcTotalMoney(totalExpenses);
     //
+    //parsea valor a ser escrito no html
+    let totalMonth1 = parseFloat(totalMonthStore1).toFixed(2);
+    let totalMonth2 = parseFloat(totalMonthStore2).toFixed(2);    
+    let expenseTotal = parseFloat(totalExpenseMonth).toFixed(2);
+
+    let clientTotal = parseInt(totalClientStore1);
+    let clientTotal2 = parseInt( totalClientStore2);
+    
+    // verifica campos zerados
+    checkValue(totalStore1, tableTotalStore1, totalMonth1)
+    checkValue(totalStore2, tableTotalStore2, totalMonth2);
+    checkValue(totalExpenses, tableTotalExpense, expenseTotal);
+    checkValue(qtClient1, tableTotalClient1, clientTotal);
+    checkValue(qtClient2,tableTotalClient2, clientTotal2);
 
     let balance = parseFloat((totalMonthStore1 + totalMonthStore2) - totalExpenseMonth).toFixed(2);
-    let tableBalance = document.querySelector(".balance")
+    
     const clientAll = totalClientStore1 + totalClientStore2;
     const tableTClient = document.querySelector(".qtTotal");
     if(balance < 0){
@@ -141,4 +47,41 @@ export const balance = () => {
     }
     tableBalance.innerHTML = balance;
     tableTClient.innerHTML = clientAll;
+}
+
+const calcTotalMoney = (arr) => {
+
+    let varTotal = 0;
+    for (let i = 0; i < arr.length; i++) {
+        
+        const total = arr[i];
+        const totalValue = total.textContent;
+        const valor = parseFloat(totalValue);
+     
+        varTotal += valor; 
+
+    }
+    
+    return varTotal;
+}
+const calcTotalQt = (arr) => {
+    let varTotal = 0;
+    for (let i = 0; i < arr.length; i++) {
+        
+        const total = arr[i];
+        const totalValue = total.textContent;
+        const valor = parseInt(totalValue);
+            
+        varTotal += valor;        
+    }
+    return varTotal;
+}
+// confirmação se existem valores para o campo
+const checkValue = (qtDiv, htmlContent, valueToAttribute) =>{
+    if(qtDiv.length == 0){
+        htmlContent.innerHTML = "0";
+    }
+    else{
+        htmlContent.innerHTML = valueToAttribute;
+    } 
 }
